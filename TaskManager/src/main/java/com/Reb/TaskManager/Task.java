b3f5d666fe37fbe5e78d97a0e8da5ad70769322f
RebEmnacin <rebqemnacin@gmail.com>
2026-09-02 12:21:49 +0800

    added getters and setters

--- TaskManager/src/main/java/com/Reb/TaskManager/Task.java
+++ TaskManager/src/main/java/com/Reb/TaskManager/Task.java
@@ -26,4 +26,44 @@ public class Task {
         this.status = status;
         this.createdAt = createdAt;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
